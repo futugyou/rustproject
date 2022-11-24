8b01396bc1775a73a3e74e217315d418640b16c9
zhaokangcheng <17871902+futugyou@users.noreply.github.com>
2022-11-24 16:55:56 +0800

    create menu in another way
    create menu in another way

--- projects/tauri-app/tauri-app-vue/src-tauri/src/main.rs
+++ projects/tauri-app/tauri-app-vue/src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{Menu, MenuEntry, Submenu, MenuItem};
+use tauri::{CustomMenuItem, Menu, MenuEntry, MenuItem, Submenu};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -12,15 +12,24 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
     tauri::Builder::default()
-        .menu(Menu::with_items([MenuEntry::Submenu(Submenu::new(
-            "File",
-            Menu::with_items([
-                MenuItem::CloseWindow.into(),
-                #[cfg(targer_os = "macos")]
-                CustomMenuItem::new("hello", "Hello").into(),
-            ]),
-        ))]))
+        // .menu(Menu::with_items([MenuEntry::Submenu(Submenu::new(
+        //     "File",
+        //     Menu::with_items([
+        //         MenuItem::CloseWindow.into(),
+        //         #[cfg(targer_os = "macos")]
+        //         CustomMenuItem::new("hello", "Hello").into(),
+        //     ]),
+        // ))]))
+        .menu(menu)
         .on_window_event(|event| match event.event() {
             tauri::WindowEvent::CloseRequested { api, .. } => {
                 api.prevent_close();
