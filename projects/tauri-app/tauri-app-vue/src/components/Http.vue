a67b7d2477652925928da6d6b4da60486d5170ee
zhaokangcheng <17871902+futugyou@users.noreply.github.com>
2022-12-02 13:16:02 +0800

    add more for tauri http api
    add more for tauri http api

--- projects/tauri-app/tauri-app-vue/src/components/Http.vue
+++ projects/tauri-app/tauri-app-vue/src/components/Http.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { getClient, Body, HttpOptions, HttpVerb } from '@tauri-apps/api/http';
+import { getClient, Body, ResponseType, HttpOptions, HttpVerb } from '@tauri-apps/api/http';
 import tool from "../tool";
 
 const greetMsg = ref("");
 const httpMethod = ref("GET")
 const httpBody = ref("")
+
+const foo = ref("")
+const bar = ref("")
+const multipart = ref(true)
 
 async function makeHttpRequest() {
     const client = await getClient().catch((e) => {
@@ -31,6 +35,26 @@ async function makeHttpRequest() {
     tool.onMessage(httpBody.value)
     client.request(options).then(tool.onMessage).catch(tool.onMessage)
 }
+
+async function doPost() {
+    const client = await getClient().catch((e) => {
+        tool.onMessage(e)
+        throw e
+    })
+
+    const result = await client.request({
+        url: 'http://localhost:3003',
+        method: 'POST',
+        body: Body.form({
+            foo: foo.value,
+            bar: bar.value
+        }),
+        headers: multipart
+            ? { 'Content-Type': 'multipart/form-data' }
+            : undefined,
+        responseType: ResponseType.Text
+    })
+}
 </script>
 
 <template>
@@ -49,5 +73,21 @@ async function makeHttpRequest() {
             <br />
             <button class="btn" id="make-request"> Make request </button>
         </form>
+    </div>
+
+    <div class="card">
+        <h3>HTTP Form</h3>
+        <div class="flex gap-2 children:grow">
+            <input class="input" v-model="foo" />
+            <input class="input" v-model="bar" />
+        </div>
+        <br />
+        <label>
+            <input type="checkbox" v-model="multipart" />
+            Multipart
+        </label>
+        <br />
+        <br />
+        <button class="btn" type="button" @click="doPost()"> Post it</button>
     </div>
 </template>
